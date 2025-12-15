82be6c33777cccda66f78af53f947c36e1326bbf
Paula Bonastre <paula.bonastreborras@servexternos.gruposantander.com>
2025-12-15 16:36:50 +0100

    update Payload to remove fields from json

--- src/main/java/com/santander/san/merchant/model/Payload.java
+++ src/main/java/com/santander/san/merchant/model/Payload.java
@@ -1,5 +1,6 @@
 package com.santander.san.merchant.model;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,5 +21,10 @@ public class Payload {
    * The Fields.
    */
   private Map<String, Object> fields;
+
+  @JsonAnyGetter
+  public Map<String, Object> fields() {
+    return fields;
+  }
 
 }
